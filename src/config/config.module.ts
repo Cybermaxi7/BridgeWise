317293a473537e6458c1f96c0c0e34a0532f3371
kamaldeen Aliyu <kamalaliyu503@gmail.com>
2026-03-25 12:51:03 +0100

    Implemented the Config Support

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,9 +1,10 @@
 import { Global, Module } from '@nestjs/common';
 import { ConfigService } from './config.service';
+import { EnvironmentConfigService } from './environment-config.service';
 
 @Global()
 @Module({
-  providers: [ConfigService],
-  exports: [ConfigService],
+  providers: [ConfigService, EnvironmentConfigService],
+  exports: [ConfigService, EnvironmentConfigService],
 })
 export class ConfigModule {}
